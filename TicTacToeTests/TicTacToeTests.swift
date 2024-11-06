8800da01a929fd08c2acc9252d8be0443cce2e7f
Camilo Medina <chriscamed@hotmail.com>
2024-11-06 13:55:32 +0100

    Initial Commit

---
+++ TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Camilo Medina on 6/11/24.
+//
+
+import Testing
+@testable import TicTacToe
+
+struct TicTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
